797eff98e6652e9cba3e47cb27ab21d4c70f80f1
Brad Yates <byates@block.xyz>
2023-12-06 10:33:41 -0500

    update day 6 with a shortcut

--- day_06/solution.py
+++ day_06/solution.py
@@ -1,11 +1,11 @@
-import re
+from math import sqrt
 import time
 
 class Part1:
     @staticmethod
     def solution(lines: list[str]) -> int:
-        times = [int(s) for s in lines[0].split(":")[1].strip().split()]
-        dists = [int(s) for s in lines[1].split(":")[1].strip().split()]
+        times = [int(s) for s in lines[0].split()[1:]]
+        dists = [int(s) for s in lines[1].split()[1:]]
         total = 1
         for index in range(len(times)):
             ways2win = 0
@@ -38,6 +38,18 @@ class Part2:
             factor = int(factor / 10)
         return max - min + 1
 
+    @staticmethod
+    def shortcut(lines: list[str]) -> int:
+        tt_s = lines[0].replace(" ", "").split(":")[1]
+        dd_s = lines[1].replace(" ", "").split(":")[1]
+        a = 1
+        b = int(tt_s)
+        c = int(dd_s)
+        # solve using the quadratic formula
+        t1 = (-b + sqrt(b**2 - 4*a*c)) / (2*a)
+        t2 = (-b - sqrt(b**2 - 4*a*c)) / (2*a)
+        return int(max(t1, t2) - min(t1, t2))
+
 #class Helper:
 
 tic = time.perf_counter()
@@ -46,8 +58,9 @@ with open("input.txt", "r") as file:
     f = file.read().splitlines()
 print(f"Part 1: {Part1.solution(f)}")
 print(f"Part 2: {Part2.solution(f)}")
+print(f"Part 2: {Part2.shortcut(f)}")
 toc = time.perf_counter()
-print(f"Took {toc - tic:0.4f} seconds")
+print(f"Took {toc - tic:0.5f} seconds")
 #Part 1: 771628
 #Part 2: 27363861
 #Took 0.0001 seconds
