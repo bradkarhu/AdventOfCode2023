e32c79b6c8ee2517dde6b1f617242e6028776b39
Brad Yates <byates@block.xyz>
2023-12-18 00:27:41 -0500

    add day 17 part 2 solution

--- day_17/solution.py
+++ day_17/solution.py
@@ -1,13 +1,24 @@
-import time
 from dijkstar import Graph, find_path
+import time
 
 class Part1:
     @staticmethod
     def solution(lines: list[str]) -> int:
+        return Helper.solve(lines, range(1, 4), 1)
+
+class Part2:
+    @staticmethod
+    def solution(lines: list[str]) -> int:
+        return Helper.solve(lines, range(1, 11), 4)
+
+def node(j, i, dir):
+    return f"{j}|{i}|{dir}"
+
+class Helper:
+    def solve(lines: list[str], rg: range, min_steps: int) -> int:
         nj = len(lines)
         ni = len(lines[0])
-        rg = range(1, 4)
-        graph = Graph()        
+        graph = Graph()
         # add start node with zero loss edges to itself in each direction
         start = node(0,0,"")        
         graph.add_edge(start, node(0,0,"V"), (0, "V"))
@@ -18,59 +29,54 @@ class Part1:
                 for x in rg: # left of current node
                     if i-x < 0: break
                     loss += int(lines[j][i-x])
+                    if x < min_steps: continue
                     graph.add_edge(node(j,i,"V"), node(j,i-x,"H"), (loss, "H"))
                     graph.add_edge(node(j,i,"H"), node(j,i-x,"H"), (loss, "H"))
                 loss = 0
                 for x in rg: # right of current node
                     if i+x >= ni: break
                     loss += int(lines[j][i+x])
+                    if x < min_steps: continue
                     graph.add_edge(node(j,i,"V"), node(j,i+x,"H"), (loss, "H"))
                     graph.add_edge(node(j,i,"H"), node(j,i+x,"H"), (loss, "H"))
                 loss = 0
                 for y in rg: # up from current node
                     if j-y < 0: break
                     loss += int(lines[j-y][i])
+                    if y < min_steps: continue
                     graph.add_edge(node(j,i,"V"), node(j-y,i,"V"), (loss, "V"))
                     graph.add_edge(node(j,i,"H"), node(j-y,i,"V"), (loss, "V"))
                 loss = 0
                 for y in rg: # down from current node
                     if j+y >= nj: break
                     loss += int(lines[j+y][i])
+                    if y < min_steps: continue
                     graph.add_edge(node(j,i,"V"), node(j+y,i,"V"), (loss, "V"))
                     graph.add_edge(node(j,i,"H"), node(j+y,i,"V"), (loss, "V"))
         #print(graph.node_count)
         #print(graph.edge_count)
-        total_cost = None
+        heat_loss = None
         shortest_path = None
         for dir in ["H", "V"]:
             try:
                 path = find_path(graph, start, node(nj-1,ni-1,dir), cost_func=Helper.cost_func)
-                if total_cost is None or path.total_cost < total_cost:
+                if heat_loss is None or heat_loss > path.total_cost:
                     shortest_path = path
-                    total_cost = path.total_cost
+                    heat_loss = path.total_cost
             except:
                 pass
         #print(shortest_path)
-        return total_cost
+        return heat_loss
 
-class Part2:
-    @staticmethod
-    def solution(lines: list[str]) -> int:
-        return 0
-
-def node(j, i, dir):
-    return f"{j}|{i}|{dir}"
-
-class Helper:
     def cost_func(u, v, edge, prev_edge):
         loss, dir = edge
         if prev_edge:
             if prev_edge[1] == dir:
                 return 1e7
-        return loss
+        return loss    
 
 tic = time.perf_counter()
-#with open("sample1.txt", "r") as file:
+#with open("sample.txt", "r") as file:
 with open("input.txt", "r") as file:
     f = file.read().splitlines()
 print(f"Part 1: {Part1.solution(f)}")
@@ -78,5 +84,5 @@ print(f"Part 2: {Part2.solution(f)}")
 toc = time.perf_counter()
 print(f"Took {toc - tic:0.4f} seconds")
 #Part 1: 1099
-#Part 2: 0
-#Took 0.6561 seconds
+#Part 2: 1266
+#Took 2.0303 seconds
